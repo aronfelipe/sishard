60ac73a04f4e591dfa2a8f63398d6716ea0f244a
felipe <aronakamoto@outlook.com>
2020-05-12 14:47:17 -0300

    Feat: Classes 15 and 16

---
+++ 16-processos/children.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+
+// Children with wait and nanosleep
+
+int main() {
+    pid_t pai, filho;
+    struct timespec tim, tim2;
+    tim.tv_nsec = 0;
+    
+    for (int i = 1; i <= 8; i++) {
+        filho = fork();
+        tim.tv_sec = i;
+
+        if (filho == 0) {
+            pai = getppid();
+            waitpid(filho = getpid());
+
+            nanosleep(&tim , &tim2);
+
+            printf("Eu sou o processo filho %d, meu pai é %d\n", filho, pai);
+            break;
+        } else {
+            wait(NULL); /* reaping parent */
+        }
+    }
+    return 0;
+}
